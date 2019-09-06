d4f67fa94ef90194f2b28d6d07f6676ea556e46f
Andres Benitez <elmonito007@yahoo.com.ar>
2019-09-06 03:57:54 -0300

    Agregar Area Admin

---
+++ Presentation/Safari.UI.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Safari.UI.Web.Areas.Admin.Controllers
+{[Authorize(Roles ="Admin")]//para entrar en admin debe estar logueado y  asignarle el rol
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
