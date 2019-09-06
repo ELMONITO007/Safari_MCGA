8f9c33da9817bed7c7a742a18fc1061d4dedd6b7
Andres Benitez <elmonito007@yahoo.com.ar>
2019-09-06 03:03:29 -0300

    Bolderplate

---
+++ Presentation/Safari.UI.Web/Constants/Application.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Safari.UI.Web.Constants
+{
+    public class Application
+    {
+        public const string Name = "Safari MVC MCGA";
+        public const string ShrotName = "Safari";
+    }
+}
